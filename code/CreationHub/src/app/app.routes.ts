f951d5c809cb9880ba67a115e500667768c00720
landrasic <lejolejo14@gmail.com>
2024-08-27 17:58:02 +0200

    Navigation component
    Added basic navigation component with styling, added two basic pages with text.

--- code/CreationHub/src/app/app.routes.ts
+++ code/CreationHub/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+  ];
